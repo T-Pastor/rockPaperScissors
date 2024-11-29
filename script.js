6a61afc480c39c2a3e7b8c333dd1c5c8c17a5b30
Tim Pastor <141689447+T-Pastor@users.noreply.github.com>
2024-11-28 19:14:18 -0500

    added UI

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let computerWins = 0;
 let humanWins = 0;
 let draws = 0;
 
+//determines the computer players choice
 function getComputerChoice() {
     let choice = Math.random() * (3.01 - 0);
 
@@ -14,45 +15,65 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Rock, Paper or Scissors?", "").toLowerCase();
+//uses the ID of the button clicked to determine the human players choice
+function buttonAction(event) {
+    let target = event.target;
+    let id = target.id;
 
-    switch(choice) {
+    switch(id) {
         case "rock":
-            return 0;
+            winner(0);
             break;
         case "paper":
-            return 1;
+            winner(1);
             break;
         case "scissors":
-            return 2;
+            winner(2);
             break;
-        default: 
-            alert("Please enter rock, paper, or scissors!");
-            getHumanChoice();
+        case "reset":
+            computerWins = 0;
+            humanWins = 0;
+            draws = 0;
+            game = 0;
+            break;
+        default:
+            alert("What are you doing?");
+            break;
     }
 }
 
+//determines winner
 function winner(humanChoice) {
     let sum = parseInt(getComputerChoice()) - parseInt(humanChoice);
 
-    if (sum == 0) {
-        alert("You tied!");
-        draws++;
-    } else if (sum == -1 || sum == 2) {
-        alert("You win");
-        humanWins++;
-    } else if (sum == 1 || sum == -2) {
-        alert("You lose, please try again");
-        computerWins++;
+    if (getWinner(computerWins, humanWins)) {
+        if (sum == 0) {
+            alert("You tied!");
+            draws++;
+        } else if (sum == -1 || sum == 2) {
+            alert("You win");
+            humanWins++;
+            getWinner(computerWins, humanWins);
+        } else if (sum == 1 || sum == -2) {
+            alert("You lose, please try again");
+            computerWins++;
+            getWinner(computerWins, humanWins);
+        }
     }
 }
 
-function reset() {
-    computerWins = 0;
-    humanWins = 0;
-    draws = 0;
-} 
+ //tracks amount of wins for each player, when a player has 5 wins, the game ends
+ function getWinner(cWins, hWins) {
+    if (cWins == 5) {
+        alert("The computer has 5 wins! The computer wins! Please press reset to play again.");
+        return 0;
+    } else if (hWins == 5){
+        alert("You have 5 wins! You win! Please press reset to play again.");
+        return 0;
+    } else {
+        return 1;
+    }
+}
 
 function getWins() {
     return humanWins;
